0443867e077425940e14a535ac9a9465e7a32ac3
liangzhuoyao <liangzhuoyao@insta360.com>
2024-05-10 14:35:40 +0800

    feat(*): add matcher and compile successfully

--- clang-tools-extra/clang-tidy/bugprone/BvaModelConstructorErrCheck.h
+++ clang-tools-extra/clang-tidy/bugprone/BvaModelConstructorErrCheck.h
@@ -21,7 +21,9 @@ class BvaModelConstructorErrCheck : public ClangTidyCheck {
 public:
   BvaModelConstructorErrCheck(StringRef Name, ClangTidyContext *Context)
       : ClangTidyCheck(Name, Context) {}
+  //定义了当分析AST（抽象语法树）时需要匹配的模式
   void registerMatchers(ast_matchers::MatchFinder *Finder) override;
+  //当匹配器找到匹配时执行的实际检查逻辑
   void check(const ast_matchers::MatchFinder::MatchResult &Result) override;
 };
 
